000474807c2de39f74f87c2e12f55878829ec71d
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-05-01 08:20:41 +0530

    React Native project init and basic setup

---
+++ app/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import {
+ View,
+ Text,
+ StyleSheet,
+ StatusBar,
+} from "react-native";
+
+export default class App extends Component {
+  constructor(props) {
+   super(props)
+  }
+
+  render() {
+    return (
+      <View><Text>Hello, World!</Text></View>
+    )
+  }
+}
---
+++ index.android.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+ } from 'react-native';
+
+ import App from "./app/App";
+
+export default class DigitalClock extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+AppRegistry.registerComponent('DigitalClock', () => DigitalClock);
